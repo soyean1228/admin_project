049e966b37499a0df23a7ea28a5e93f3ea38cc54
soyean1228 <41774392+soyean1228@users.noreply.github.com>
2020-07-29 17:34:15 +0900

    Update views.py

--- mysite/myApp/views.py
+++ mysite/myApp/views.py
@@ -355,52 +355,52 @@ def modify_proposal(request):
     proposal_data = Proposal.objects.all()
     return render(request, 'myApp/proposal.html',  { "isSave" : isSuccess , "proposal_data" : proposal_data })  
 
-def select_table(request, table_name):
-    if(table_name == 'employee'):
-        employee_data = Employee.objects.all() 
-        return render(request, 'myApp/employee.html', { "employee_data" : employee_data })
-    if(table_name == 'samsung_code'):
-        samsung_code_data = SamsungCode.objects.all() 
-        return render(request, 'myApp/samsung_code.html', { "samsung_code_data" : samsung_code_data })
-    if(table_name == 'authority'):
-        authority_data = Authority.objects.all() 
-        return render(request, 'myApp/authority_select.html', { "authority_data" : authority_data })
-    if(table_name == 'product'):
-        product_data = Product.objects.all()
-        return render(request, 'myApp/product.html', { "product_data" : product_data })
-    if(table_name == 'customer'):
-        customer_data = Customer.objects.all()
-        return render(request, 'myApp/customer_select.html', { "customer_data" : customer_data })
-    if(table_name == 'customer_purchasing'):
-        customer_purchasing_data = CustomerPurchasing.objects.all()
-        return render(request, 'myApp/customer_purchasing_select.html', { "customer_purchasing_data" : customer_purchasing_data })
-    if(table_name == 'customer_settlement'):
-        customer_settlement_data = CustomerSettlement.objects.all()
-        return render(request, 'myApp/customer_settlement_select.html', { "customer_settlement_data" : customer_settlement_data })
-    if(table_name == 'co_salesman'):
-        co_salesman_data = CoSalesman.objects.all()
-        return render(request, 'myApp/co_salesman_select.html', { "co_salesman_data" : co_salesman_data })
-    if(table_name == 'broker'):
-        broker_data = Broker.objects.all()
-        return render(request, 'myApp/broker_select.html', { "broker_data" : broker_data })
-    if(table_name == 'sales'):
-        sales_data = Sales.objects.all()
-        return render(request, 'myApp/sales_select.html', { "sales_data" : sales_data })
-    if(table_name == 'sales_content'):
-        sales_content_data = SalesContent.objects.all()
-        return render(request, 'myApp/sales_content_select.html', { "sales_content_data" : sales_content_data })
-    if(table_name == 'approval'):
-        approval_data = Approval.objects.all()
-        return render(request, 'myApp/approval_select.html', { "approval_data" : approval_data })
-    if(table_name == 'order'):
-        order_data = OrderData.objects.all() 
-        return render(request, 'myApp/order_select.html', { "order_data" : order_data })
-    if(table_name == 'deposit'):
-        deposit_data = Deposit.objects.all()
-        return render(request, 'myApp/deposit_select.html', { "deposit_data" : deposit_data })
-    if(table_name == 'delivery'):
-        delivery_data = Delivery.objects.all()
-        return render(request, 'myApp/delivery_select.html', { "delivery_data" : delivery_data })
+# def select_table(request, table_name):
+#     if(table_name == 'employee'):
+#         employee_data = Employee.objects.all() 
+#         return render(request, 'myApp/employee.html', { "employee_data" : employee_data })
+#     if(table_name == 'samsung_code'):
+#         samsung_code_data = SamsungCode.objects.all() 
+#         return render(request, 'myApp/samsung_code.html', { "samsung_code_data" : samsung_code_data })
+#     if(table_name == 'authority'):
+#         authority_data = Authority.objects.all() 
+#         return render(request, 'myApp/authority_select.html', { "authority_data" : authority_data })
+#     if(table_name == 'product'):
+#         product_data = Product.objects.all()
+#         return render(request, 'myApp/product.html', { "product_data" : product_data })
+#     if(table_name == 'customer'):
+#         customer_data = Customer.objects.all()
+#         return render(request, 'myApp/customer_select.html', { "customer_data" : customer_data })
+#     if(table_name == 'customer_purchasing'):
+#         customer_purchasing_data = CustomerPurchasing.objects.all()
+#         return render(request, 'myApp/customer_purchasing_select.html', { "customer_purchasing_data" : customer_purchasing_data })
+#     if(table_name == 'customer_settlement'):
+#         customer_settlement_data = CustomerSettlement.objects.all()
+#         return render(request, 'myApp/customer_settlement_select.html', { "customer_settlement_data" : customer_settlement_data })
+#     if(table_name == 'co_salesman'):
+#         co_salesman_data = CoSalesman.objects.all()
+#         return render(request, 'myApp/co_salesman_select.html', { "co_salesman_data" : co_salesman_data })
+#     if(table_name == 'broker'):
+#         broker_data = Broker.objects.all()
+#         return render(request, 'myApp/broker_select.html', { "broker_data" : broker_data })
+#     if(table_name == 'sales'):
+#         sales_data = Sales.objects.all()
+#         return render(request, 'myApp/sales_select.html', { "sales_data" : sales_data })
+#     if(table_name == 'sales_content'):
+#         sales_content_data = SalesContent.objects.all()
+#         return render(request, 'myApp/sales_content_select.html', { "sales_content_data" : sales_content_data })
+#     if(table_name == 'approval'):
+#         approval_data = Approval.objects.all()
+#         return render(request, 'myApp/approval_select.html', { "approval_data" : approval_data })
+#     if(table_name == 'order'):
+#         order_data = OrderData.objects.all() 
+#         return render(request, 'myApp/order_select.html', { "order_data" : order_data })
+#     if(table_name == 'deposit'):
+#         deposit_data = Deposit.objects.all()
+#         return render(request, 'myApp/deposit_select.html', { "deposit_data" : deposit_data })
+#     if(table_name == 'delivery'):
+#         delivery_data = Delivery.objects.all()
+#         return render(request, 'myApp/delivery_select.html', { "delivery_data" : delivery_data })
 
 def select(request):
     return render(request, 'myApp/select.html')
@@ -410,50 +410,50 @@ def select_result(request):
     data = scm_select.select(request)
     return render(request, 'myApp/select_result.html', { "data" : data })
 
-def order_upload(request):
-    if request.method == 'POST':
-        if 'file' in request.FILES:
-            wb = load_workbook(filename=request.FILES['file'].file)
-            first_sheet = wb.get_sheet_names()[0]
-            worksheet = wb.get_sheet_by_name(first_sheet)
-            print(worksheet)
+# def order_upload(request):
+#     if request.method == 'POST':
+#         if 'file' in request.FILES:
+#             wb = load_workbook(filename=request.FILES['file'].file)
+#             first_sheet = wb.get_sheet_names()[0]
+#             worksheet = wb.get_sheet_by_name(first_sheet)
+#             print(worksheet)
             
-            for row in worksheet.iter_rows(min_row=2): # Offset for header
-                order = OrderData()
-                order.order_num = row[0].value
-                order.sales_num = row[1].value
-                order.product_model_name = row[2].value
-                order.order_quantity = row[3].value
-                order.order_date = row[4].value
-                order.quote_num = row[5].value
-                order.scheduled_delivery_date = row[6].value
-                order.assignment = row[7].value
-                order.recipient = row[8].value
-                order.recipient_phone = row[9].value
-                order.delivery_address = row[10].value
-                order.balance = row[11].value
-                order.order_close = row[12].value
-                print(order.balance)
+#             for row in worksheet.iter_rows(min_row=2): # Offset for header
+#                 order = OrderData()
+#                 order.order_num = row[0].value
+#                 order.sales_num = row[1].value
+#                 order.product_model_name = row[2].value
+#                 order.order_quantity = row[3].value
+#                 order.order_date = row[4].value
+#                 order.quote_num = row[5].value
+#                 order.scheduled_delivery_date = row[6].value
+#                 order.assignment = row[7].value
+#                 order.recipient = row[8].value
+#                 order.recipient_phone = row[9].value
+#                 order.delivery_address = row[10].value
+#                 order.balance = row[11].value
+#                 order.order_close = row[12].value
+#                 print(order.balance)
 
-                if order.balance == None : 
-                    # 입력이 안되었으면 자동으로 계산되도록 
-                    # 수량 = 승인수량 - 주문수량
-                    # balance = Approval.approval_quantity - order_quantity 
-                    try :
-                        row = Approval.objects.get(sales_num=order.sales_num)
-                        if order.order_quantity != None and row.approval_quantity != None:  
-                            order.balance = row.approval_quantity - order.order_quantity
-                        elif row.approval_quantity != None and order.order_quantity == None:
-                            order.balance = row.approval_quantity
-                        else:
-                            order.balance = None
-                    except ObjectDoesNotExist:
-                        print("예외")
-                        order.balance = None
+#                 if order.balance == None : 
+#                     # 입력이 안되었으면 자동으로 계산되도록 
+#                     # 수량 = 승인수량 - 주문수량
+#                     # balance = Approval.approval_quantity - order_quantity 
+#                     try :
+#                         row = Approval.objects.get(sales_num=order.sales_num)
+#                         if order.order_quantity != None and row.approval_quantity != None:  
+#                             order.balance = row.approval_quantity - order.order_quantity
+#                         elif row.approval_quantity != None and order.order_quantity == None:
+#                             order.balance = row.approval_quantity
+#                         else:
+#                             order.balance = None
+#                     except ObjectDoesNotExist:
+#                         print("예외")
+#                         order.balance = None
                 
-                if order.order_num != None:
-                    order.save()
+#                 if order.order_num != None:
+#                     order.save()
 
-        order_data = OrderData.objects.all() 
-        print(order_data)
-        return render(request, 'myApp/order_result.html', { "order_data" : order_data })
+#         order_data = OrderData.objects.all() 
+#         print(order_data)
+#         return render(request, 'myApp/order_result.html', { "order_data" : order_data })
